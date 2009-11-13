21e3b13b4cb0cd0e3cdbd43054dfd3f0a5256bba
pendergast.gary <pendergast.gary@4b7f0800-c249-11de-ad5a-915b6f3f5ed0>
2009-11-13 17:06:28 +0000

    Clarified gettext domain

--- job-manager.php
+++ job-manager.php
@@ -6,6 +6,7 @@ Description: A job and job application management plugin for Wordpress.
 Version: 0.2.1
 Author: Gary Pendergast
 Author URI: http://pento.net/
+Text domain: jobman
 Tags: job, jobs, manager, list, listing, employment, employer
 */
 
