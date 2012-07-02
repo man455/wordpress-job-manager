421754ef007f25bd7048073dda4b35135fc28698
pendergast.gary <pendergast.gary@4b7f0800-c249-11de-ad5a-915b6f3f5ed0>
2012-07-02 08:52:50 +0000

    It's a real plugin! (That does nothing.)

---
+++ job-manager.php
@@ -0,0 +1,56 @@
+<?php //encoding: utf-8
+/*
+Plugin Name: Job Manager
+Plugin URI: http://pento.net/projects/wordpress-job-manager-plugin/
+Description: A job listing and job application management plugin for WordPress.
+Version: 1.0
+Author: Gary Pendergast
+Author URI: http://pento.net/
+Text Domain: jobman
+Tags: job, jobs, manager, list, listing, employment, employer, career
+*/
+
+/*
+	Copyright 2009, 2010 Gary Pendergast (http://pento.net/)
+	Copyright 2010 Automattic (http://automattic.com/)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// Version
+define( 'JOBMAN_VERSION', '1.0' );
+define( 'JOBMAN_DB_VERSION', 19 );
+
+// Define the URL to the plugin folder
+define( 'JOBMAN_FOLDER', 'job-manager' );
+if( ! defined( 'JOBMAN_URL' ) )
+	define( 'JOBMAN_URL', WP_PLUGIN_URL . '/' . JOBMAN_FOLDER );
+
+// Define the basename
+define( 'JOBMAN_BASENAME', plugin_basename( __FILE__ ) );
+
+// Define the complete directory path
+define( 'JOBMAN_DIR', dirname( __FILE__ ) );
+
+// Load scbFramework
+require dirname( __FILE__ ) . '/scb/load.php';
+
+function jobman_init() {
+	$options = new scbOptions( 'my_plugin', __FILE__ );
+}
+
+scb_init( 'jobman_init' );
+
+?>
