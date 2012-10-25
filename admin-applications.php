31fb1ef471d52e3fe5717235343374adeb76c3da
gary <gary@pento.net>
2012-10-25 23:05:46 +0000

    should be using esc_*(), not htmlspecialchars()

--- admin-applications.php
+++ admin-applications.php
@@ -51,14 +51,14 @@ function jobman_list_applications() {
 				<table>
 					<tr>
 						<th scope="row"><?php _e( 'Job ID', 'jobman' ) ?>:</th>
-						<td><input type="text" name="jobman-jobid" value="<?php echo ( array_key_exists( 'jobman-jobid', $_REQUEST ) )?( htmlspecialchars($_REQUEST['jobman-jobid']) ):( '' ) ?>" /></td>
+						<td><input type="text" name="jobman-jobid" value="<?php echo ( array_key_exists( 'jobman-jobid', $_REQUEST ) )?( esc_attr( $_REQUEST['jobman-jobid'] ) ):( '' ) ?>" /></td>
 					</tr>
 <?php
 	if( $options['user_registration'] ) {
 ?>
 					<tr>
 						<th scope="row"><?php _e( 'Registered Applicant', 'jobman' ) ?>:</th>
-						<td><input type="text" name="jobman-applicant" value="<?php echo ( array_key_exists( 'jobman-applicant', $_REQUEST ) )?( htmlspecialchars($_REQUEST['jobman-applicant']) ):( '' ) ?>" /></td>
+						<td><input type="text" name="jobman-applicant" value="<?php echo ( array_key_exists( 'jobman-applicant', $_REQUEST ) )?( esc_attr( $_REQUEST['jobman-applicant'] ) ):( '' ) ?>" /></td>
 					</tr>
 <?php
 	}
@@ -133,10 +133,10 @@ function jobman_list_applications() {
 			switch( $field['type'] ) {
 				case 'text':
 				case 'textarea':
-						echo "<td><input type='text' name='jobman-field-$id' value='".htmlspecialchars($req_value)."' /></td>";
+						echo "<td><input type='text' name='jobman-field-$id' value='" . esc_attr( $req_value ) . "' /></td>";
 					break;
 				case 'date':
-					echo "<td><input type='text' class='datepicker' name='jobman-field-$id' value='".htmlspecialchars($req_value)."' /></td>";
+					echo "<td><input type='text' class='datepicker' name='jobman-field-$id' value='" . esc_attr( $req_value ) . "' /></td>";
 					break;
 				case 'radio':
 				case 'checkbox':
@@ -482,7 +482,7 @@ function jobman_list_applications() {
 							}
 						}
 ?>
-				<td><?php echo htmlspecialchars($data) ?></td>
+				<td><?php echo esc_html( $data ) ?></td>
 <?php
 					}
 				}
@@ -676,7 +676,7 @@ function jobman_application_display_details( $appid ) {
 					case 'date':
 					case 'textarea':
 					case 'select':
-						echo htmlspecialchars($item);
+						echo esc_html( $item );
 						break;
 					case 'file':
 						$fileurl = wp_get_attachment_url( $item );
@@ -684,7 +684,7 @@ function jobman_application_display_details( $appid ) {
 							echo "<a href='$fileurl'>" . __( 'Download', 'jobman' ) . "</a>";
 						break;
 					case 'geoloc':
-						echo '<a href="http://maps.google.com/maps?q=' . urlencode( $item ) . '">' . htmlspecialchars($appdata['data-display'.$fid]) . ' (' . htmlspecialchars($item) . ')</a>';
+						echo '<a href="http://maps.google.com/maps?q=' . urlencode( $item ) . '">' . esc_html( $appdata['data-display'.$fid] ) . ' (' . esc_html( $item ) . ')</a>';
 						break;
 				}
 				if( $fid == $fromid ) {
